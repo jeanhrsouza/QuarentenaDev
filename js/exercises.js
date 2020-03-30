c70441bb1a899d96d7efb6c39e73d2b4bf9335aa
Jean Souza <jeanhrsouza@hotmail.com>
2020-03-30 20:41:58 -0300

    Learning alert

---
+++ js/exercises.js
@@ -0,0 +1,16 @@
+
+
+const sumChicken = () => {
+    total = 300 / 15;
+    alert(`Cada galinha custou  R$${total}`);
+}
+
+const sumCandy = () => {
+    total = 19 + 45;
+    alert(`Maria tem  ${total} balas`)
+}
+
+const sumPi = () => {
+    area = 3.14 * 2 * 2;
+    alert(`A área é de ${area}`);
+}
